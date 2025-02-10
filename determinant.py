bfb962d5da85556989dbd5061f2a47985b33958b
Umarov Gadzimurad <dostoyanie1977yndex.com@gmail.com>
2025-02-10 09:44:10 +0300

    initial commit

--- determinant.py
+++ determinant.py
@@ -2,13 +2,13 @@
 
 
 def det_3x3(matrix):
-    size = len(matrix)
+    size_1 = len(matrix)
 
-    main_diag = functools.reduce(lambda x, y: x * y, [matrix[i][i] for i in range(size)])
+    main_diag = functools.reduce(lambda x, y: x * y, [matrix[0][0], matrix[1][1], matrix[2][2]])
     second_diag = functools.reduce(lambda x, y: x * y, [matrix[0][1], matrix[1][2], matrix[2][0]])
     third_diag = functools.reduce(lambda x, y: x * y, [matrix[0][2], matrix[1][0], matrix[2][1]])
 
-    anti_diag = functools.reduce(lambda x, y: x * y, [matrix[i][size - i - 1] for i in range(size)])
+    anti_diag = functools.reduce(lambda x, y: x * y, [matrix[0][2], matrix[1][1], matrix[2][0]])
     second_anti_diag = functools.reduce(lambda x, y: x * y, [matrix[0][0], matrix[1][2], matrix[2][1]])
     third_anti_diag = functools.reduce(lambda x, y: x * y, [matrix[0][1], matrix[1][0], matrix[2][2]])
 
@@ -29,8 +29,6 @@ while True:
             "Все элементы матрицы должны быть числом, а также количество элементов должно быть размерности матрицы в квадрате")
         list_of_elements = (input("Введите матрицу через пробел: ")).split()
         check = [i.isdigit() for i in list_of_elements]
-        if [i != False for i in check] and : #исправить проверку
-            break
     list_of_elements = [int(i) for i in list_of_elements]
     matrix = [list_of_elements[i * size: (i + 1) * size] for i in range(size)]
     if size == 2:
@@ -41,16 +39,24 @@ while True:
         print(det_3x3(matrix))
     elif size == 4:
         minor = []
-        temp_matrix = [[],[],[]]
-        k, r = 0, 0
+        temp_matrix = []
+        k, result = 0, []
         for d in range(size):
-            for i in range(1, size):
+            for i in range(size):
                 for j in range(size):
-                    if j != k:
-                        temp_matrix[i - 1].append(matrix[i][j])
-                k += 1
+                        if j != k and i != 0:
+                            minor.append(matrix[i][j])
 
-            minor.append(det_3x3(temp_matrix))
-            temp_matrix = [[],[],[]]
-        print(sum(minor), minor)
+            k+=1
+            for l in range(3):
+                temp_matrix.append(minor[l * 3:(l+1)*3])
+
+            minor = []
+
+        for i in range(4):
+            temp_matrix_2 = temp_matrix[i * 3:(i+1)*3]
+            result.append((-1)**i*(det_3x3(temp_matrix_2))*matrix[0][i])
+        print(sum(result))
+
     break
+#1 3 2 4 5 4 6 7 1 1 2 3 4 5 4 3
